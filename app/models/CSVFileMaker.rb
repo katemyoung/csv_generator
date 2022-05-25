85bb814b8dd22ffcdf20e91d17529820d0c035e4
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-05-25 22:29:35 +0200

    Add method #generate_csv_data to CSVFileMaker

--- app/models/CSVFileMaker.rb
+++ app/models/CSVFileMaker.rb
@@ -1,6 +1,9 @@
 class CSVFileMaker
   require "csv"
 
+  def generate_csv_data
+  end
+
   def generate_file(file_path)
     CSV.open("#{file_path}", "w") do |csv|
       csv << ["1234567"]
--- spec/models/csv_file_maker_spec.rb
+++ spec/models/csv_file_maker_spec.rb
@@ -2,11 +2,15 @@ require "rails_helper"
 require "csv"
 
 describe CSVFileMaker do
+  let(:csv_maker) { CSVFileMaker.new }
+  let(:file_path) { "./tmp/test.csv"}
+  let(:file) { csv_maker.generate_file(file_path) }
+  let(:csv_data) { csv_maker.generate_csv_data }
+
+  it { is_expected.to respond_to(:generate_csv_data) }
+
   describe "#generate_file" do
     after(:context) { File.delete("./tmp/test.csv") }
-    let(:csv_maker) { CSVFileMaker.new }
-    let(:file_path) { "./tmp/test.csv"}
-    let(:file) { csv_maker.generate_file(file_path) }
     
     it "outputs a csv file" do
       expect(file).to be_a(CSV)
@@ -16,8 +20,8 @@ describe CSVFileMaker do
       expect(csv_maker).to respond_to(:generate_file).with(1).argument 
     end
  
-    it "outputs a csv file containing the employers account number" do
-      expect(CSV.read("#{file_path}")).to include(["1234567"])
+    it "outputs a csv data contains the employer's account number" do
+      expect(CSV.read("#{file_path}")).to include(["1234567"]) 
     end
   end
 end
