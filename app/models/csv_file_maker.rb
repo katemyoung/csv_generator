af8c3a7c2ccf898e0695f65f9b7edcac8f69ae7c
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-05-30 21:33:06 +0200

    Feature completed: csv data employer created from Employer class

--- app/models/csv_file_maker.rb
+++ app/models/csv_file_maker.rb
@@ -1,10 +1,13 @@
 class CsvFileMaker
   require "csv"
-  attr_accessor :employer_name, :employer_account_number
+  attr_accessor :employer_name
   
   def initialize(employer_name)
     @employer_name = employer_name
-    @employer_account_number = "1234567" # get this using Employer.find_by(name: employer_name)
+  end
+
+  def employer_account_number
+    Employer.find_by(name: @employer_name).account_number.to_s
   end
 
   def generate_csv_data
--- spec/models/csv_file_maker_spec.rb
+++ spec/models/csv_file_maker_spec.rb
@@ -2,27 +2,30 @@ require "rails_helper"
 require "csv"
 
 describe CsvFileMaker do
-  let(:csv_maker) { CsvFileMaker.new("My Company") }
+  let(:account_number) { "123456"}
+  let(:name) { "My Company" }
+  let(:employer) { Employer.create(account_number: account_number, name: name) }
+  let(:csv_maker) { CsvFileMaker.new(name)}
   let(:file_path) { "./tmp/test.csv"}
   let(:file) { csv_maker.generate_file(file_path) }
   let(:csv_data) { csv_maker.generate_csv_data }
 
   describe "a new CsvFileMaker" do
     it "is assocaited with an employer account " do
-      employer = double(name: "My Company", account_number: "1234567")
+      employer = Employer.create(account_number: account_number, name: name)
       expect(csv_maker.employer_name).to eq(employer.name)
     end
 
     it "finds the accociated employee acount number" do
-      employer = double(name: "Another Company", account_number: "1111111")
-      expect(csv_maker.employer_account_number).to eq(employer.account_number)
+      employer = Employer.create(account_number: account_number, name: name)
+      expect(csv_maker.employer_account_number).to eq(employer.account_number.to_s)
     end
   end
 
   describe "#employer_account_number" do
     it "returns the employer's account number" do
-      employer = double(account_number: "1234567")
-      expect(csv_maker.employer_account_number).to eq("1234567")
+      employer = Employer.create(account_number: account_number, name: name)
+      expect(csv_maker.employer_account_number).to eq(employer.account_number.to_s)
     end
   end
 
@@ -31,15 +34,17 @@ describe CsvFileMaker do
     let(:year) { "2022" }
 
     it "returns an array" do
+      employer = Employer.create(account_number: account_number, name: name)
       expect(csv_data).to be_an(Array)
     end
 
     it "contains the employer's account number" do
-      employer = double(account_number: "1234567")
-      expect(csv_data).to include(employer.account_number) 
+      employer = Employer.create(account_number: account_number, name: name)
+      expect(csv_data).to include(employer.account_number.to_s) 
     end
 
     it "contains the year" do
+      employer = Employer.create(account_number: account_number, name: name)
       expect(csv_data).to include(year) 
     end
 
@@ -49,6 +54,7 @@ describe CsvFileMaker do
     after(:context) { File.delete("./tmp/test.csv") }
     
     it "outputs a csv file" do
+      employer = Employer.create(account_number: account_number, name: name)
       expect(file).to be_a(CSV)
     end
 
@@ -57,6 +63,7 @@ describe CsvFileMaker do
     end
  
     it "outputs a csv data contains the employer's account number" do
+      employer = Employer.create(account_number: account_number, name: name)
       expect(CSV.read("#{file_path}")).to include(csv_data) 
     end
   end
