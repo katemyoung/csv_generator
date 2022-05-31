8547f9c01f0acf26bb9b1a7979a5901c47540536
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-05-31 11:48:40 +0200

    Refactor employer_spec.rb to use factory test data instead of real objects

---
+++ spec/factories/employers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :employer do
+    account_number { 1234567 }
+    name { "My Company" }
+  end
+end
--- spec/models/employer_spec.rb
+++ spec/models/employer_spec.rb
@@ -1,24 +1,17 @@
 require "rails_helper"
 
 describe Employer do
-  let(:account_number) { "123456"}
   let(:name) { "My Company" }
-  let(:employer) { Employer.create(account_number: account_number, name: name) }
+  let(:account_number) { 1234567 }
+  let(:employer) { FactoryBot.build_stubbed(:employer) }
 
   describe "initialisation" do
     it "has an account number" do
-      expect(employer).to respond_to(:account_number)
+      expect(employer.account_number).to eq(account_number)
     end
 
     it "has an employer name" do
-      expect(employer).to respond_to(:name) 
-    end
-  end
-
-  describe ".find_by" do
-    it "returns an account number as a string given an employer name" do
-      employer_record = Employer.find_by(name: employer.name)
-      expect(employer_record.account_number.to_s).to eq(employer.account_number.to_s)
+      expect(employer.name).to eq(name)
     end
   end
 end
