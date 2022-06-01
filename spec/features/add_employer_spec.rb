65cd65eda2fac46f67314d84407f0d294a807625
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-06-01 16:45:30 +0200

    Add from to employers/new.html.erb

--- spec/features/add_employer_spec.rb
+++ spec/features/add_employer_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "adding an employer" do
   it "allows a user to create an employer with name and eNapsa account number" do
     visit new_employer_path
-    fill_in "Employer Name", with: "My Compnay"
-    fill_in "eNapsa Account Number", with: 1234567 
+    fill_in "Name", with: "My Compnay"
+    fill_in "Account number", with: 1234567 
     click_on "Create Employer"
     visit employers_path
     expect(page).to have_content("My Compnay")
