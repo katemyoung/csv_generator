7673777f464f7b9ab0544f3c642d6a13c36b48e4
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-06-02 12:37:41 +0200

    Complete feature create employer

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -2,4 +2,13 @@ class EmployersController < ApplicationController
   def new
     @employer = Employer.new
   end
+
+  def index
+    @employers = Employer.all
+  end 
+
+  def create
+    employer = Employer.create(name: params[:employer][:name], account_number: params[:employer][:account_number])
+    redirect_to employers_path
+  end
 end
--- spec/features/add_employer_spec.rb
+++ spec/features/add_employer_spec.rb
@@ -3,11 +3,10 @@ require "rails_helper"
 RSpec.describe "adding an employer" do
   it "allows a user to create an employer with name and eNapsa account number" do
     visit new_employer_path
-    fill_in "Name", with: "My Compnay"
+    fill_in "Name", with: "My Company"
     fill_in "Account number", with: 1234567 
     click_on "Create Employer"
-    visit employers_path
-    expect(page).to have_content("My Compnay")
+    expect(page).to have_content("My Company")
     expect(page).to have_content(1234567)
   end
 end
