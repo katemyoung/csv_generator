c0dbcc74a8e78f78e955dbe71961257ac38f716e
Connor McQuillan <connor@thoughtbot.com>
2022-06-09 09:30:55 +0100

    Refactor `CsvFileMaker` specs
    Before, the specs used `let` extensively.
    
    This introduced some mystery guests that made the tests harder to read.
    
    We refactored the specs to "Let's Not" and wrote some Four-Phase tests.
    We also refactored the spec so that a `CSV` is not written, which
    simplified the test.
    
    Co-authored-by: Kate Young <89363807+katemyoung@users.noreply.github.com>

--- spec/models/csv_file_maker_spec.rb
+++ spec/models/csv_file_maker_spec.rb
@@ -1,68 +1,19 @@
 require "rails_helper"
-require "csv"
 
 describe CsvFileMaker do
-  let(:account_number) { "123456"}
-  let(:name) { "My Company" }
-  let(:csv_maker) { CsvFileMaker.new(name)}
-  let(:year) { "2022" }
-
-  describe "a new CsvFileMaker" do
-    it "is associated with an employer account " do
-      employer = Employer.new(name: name)
-      allow(employer).to receive(:name).and_return(name)
-      expect(csv_maker.employer_name).to eq(employer.name)
-    end
-  end
-
-  describe "#employer_account_number" do
-    it "finds the associated employee acount number" do
-      allow(Employer).to receive(:find_by).and_return(Employer.new(account_number: account_number))
-      employer = Employer.find_by
-      expect(csv_maker.employer_account_number).to eq(employer.account_number.to_s)
-    end
-  end
-
-  describe "#generate_csv_data" do
-  let(:csv_data) { csv_maker.generate_csv_data }
-
-    it "returns an array" do
-      allow(csv_maker).to receive(:employer_account_number).and_return(account_number)
-      expect(csv_data).to be_an(Array)
-    end
-
-    it "contains the employer's account number" do
-      allow(csv_maker).to receive(:employer_account_number).and_return(account_number)
-      expect(csv_data).to include(account_number.to_s) 
-    end
-
-    it "contains the year" do
-      allow(csv_maker).to receive(:employer_account_number).and_return(account_number)
-      expect(csv_data).to include(year) 
-    end
-
-  end
-
   describe "#generate_file" do
-    let(:file_path) { "./tmp/test.csv"}
-    let(:file) { csv_maker.generate_file(file_path) }
-    let(:csv_data) { [account_number, year] }
-    let(:generate_csv_data) { csv_data  }
+    it "writes the data to a csv file" do
+      employer = create(:employer, account_number: 123)
+      file_path = "file_path"
+      csv_file = instance_double(CSV)
+      allow(CSV).to receive(:open).with(file_path, "w").and_yield(csv_file)
+      allow(csv_file).to receive(:add_row)
+      csv_file_maker = CsvFileMaker.new(employer.name)
 
-    after(:context) { File.delete("./tmp/test.csv") }
-    
-    it "outputs a csv file" do
-      allow(csv_maker).to receive(:generate_csv_data).and_return(csv_data)
-      expect(file).to be_a(CSV)
-    end
+      csv_file_maker.generate_file(file_path)
 
-    it "takes a file path and name as an argument" do
-      expect(csv_maker).to respond_to(:generate_file).with(1).argument 
-    end
- 
-    it "outputs a csv data contains the employer's account number" do
-      allow(csv_maker).to receive(:generate_csv_data).and_return(csv_data)
-      expect(CSV.read("#{file_path}")).to include(csv_data) 
+      expect(CSV).to have_received(:open).with(file_path, "w").once
+      expect(csv_file).to have_received(:add_row).with(["123", "2022"]).once
     end
   end
 end
