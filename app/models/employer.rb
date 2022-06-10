731f9d7437ce043618241f4cbee8994410208c02
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-06-10 11:46:28 +0200

    Add validation for employer name
    Employer cannot be created without a name, and new employer form is rendered and an error message displayed is this is attempted

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,3 +1,4 @@
 class Employer < ApplicationRecord
   validates :account_number, length: {is: 7}
+  validates :name, presence: true
 end
--- spec/features/add_employer_spec.rb
+++ spec/features/add_employer_spec.rb
@@ -1,16 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "adding an employer" do
+  context "valid inputs" do
   it "allows a user to create an employer with name and eNapsa account number" do
-    visit new_employer_path
-    fill_in "Name", with: "My Company"
-    fill_in "Account number", with: 1234567
-    click_on "Create Employer"
-    expect(page).to have_content("My Company")
-    expect(page).to have_content(1234567)
+      visit new_employer_path
+      fill_in "Name", with: "My Company"
+      fill_in "Account number", with: 1234567
+      click_on "Create Employer"
+      expect(page).to have_content("My Company")
+      expect(page).to have_content(1234567)
+    end
   end
 
-  context "with invlid inputs" do
+  context "with invlaid input: no account number" do
     it "does not allow a user to create an employer without an eNapsa account number" do
       visit new_employer_path
       fill_in "Name", with: "My Company"
@@ -18,18 +20,27 @@ RSpec.describe "adding an employer" do
       expect(page).not_to have_content("My Company")
     end
 
-    it "rerenders the new employer form if no account number is supplied" do
-      visit new_employer_path
-      fill_in "Name", with: "My Company"
-      click_on "Create Employer"
-      expect(page).to have_content("New Employer")
-    end
-
-    it "dispalys an error message if an invalid or no eNapsa account number is supplied" do
+    it "rerenders the new employer form and dispalys an error message if an invalid or no eNapsa account number is supplied" do
       visit new_employer_path
       fill_in "Name", with: "My Company"
       click_on "Create Employer"
       expect(page).to have_content("Account number is the wrong length (should be 7 characters)")
+    end
+  end
+
+  context "with invlaid input: no name" do
+    it "does not allow a user to create an employer without an name" do
+      visit new_employer_path
+      fill_in "Account number", with: 1234567
+      click_on "Create Employer"
+      expect(page).not_to have_content("1234567")
+    end
+
+    it "rerenders the new employer form and dispalys an error message if no name is supplied" do
+      visit new_employer_path
+      fill_in "Account number", with: 1234567
+      click_on "Create Employer"
+      expect(page).to have_content("Name can't be blank")
     end
   end
 end
