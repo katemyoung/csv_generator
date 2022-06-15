712ef187b674e4f623060316d4e8ade4cdc749de
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-06-15 17:47:44 +0200

    Add feature test to TDD the csv eNapsa monthly return download feature

--- spec/features/create_enapsa_monthly_report.rb
+++ spec/features/create_enapsa_monthly_report.rb
@@ -1,8 +1,17 @@
 require "rails_helper"
 
 describe "creating a csv report" do
-  it "allows a user to generate a csv file report" do
-    visit "/enapsa_monthly_report/new"
-    expect(page).to have_content("New eNapsa Report")
+  describe "suggested return date is correct" do
+    it "allows a user to generate a csv file report" do
+      visit "/enapsa_monthly_report/new"
+      fill_in "Employer", with: "My Company" # can change to dropdown menu
+      # date will be auto selected
+      click_on "Generate report" 
+      expect(page).to have_content("Click here to download your file")
+      # expect file download
+    end
+  end
+
+  xdescribe "custom return date is required" do
   end
 end
