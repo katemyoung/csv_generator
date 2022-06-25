bea7e8b04c38b28b56175730e95b4fc6e5d052ac
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-06-25 12:31:41 +0200

    EnapsaMonthlyReport generates and includes in csv report a dynamic value for month

--- app/models/enapsa_monthly_report.rb
+++ app/models/enapsa_monthly_report.rb
@@ -2,8 +2,9 @@ class EnapsaMonthlyReport
   require "csv"
   attr_accessor :employer_name
 
-  def initialize(employer_name)
+  def initialize(employer_name, month=(Time.now.mon - 1))
     @employer_name = employer_name
+    @month = month
   end
 
   def generate_file
@@ -14,12 +15,14 @@ class EnapsaMonthlyReport
 
   private
 
+  attr_accessor :month
+
   def employer_account_number
     Employer.find_by(name: @employer_name).account_number.to_s
   end
 
   def generate_csv_data
-    [employer_account_number, "2022"]
+    [employer_account_number, "2022", month.to_s]
   end
 
   def file_path
--- spec/features/create_enapsa_monthly_report_spec.rb
+++ spec/features/create_enapsa_monthly_report_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
-
-describe "creating an eNapsa monthly report" do
-  describe "year only" do
+RSpec.describe "creating an eNapsa monthly report" do
+  describe "suggested return date is correct for months Feb to Dec" do
     it "allows a user to download a csv report" do
       employer = create(:employer, account_number: 1111111, name: "My Company")
+      month = (Time.now.mon - 1).to_s
 
       visit "/enapsa_monthly_report/new"
       fill_in "Employer", with: "My Company"
@@ -12,17 +12,13 @@ describe "creating an eNapsa monthly report" do
       click_on "Download eNapsa Report"
       csv = CSV.parse(page.body).transpose
 
-      expect(csv).to eq([["1111111"], ["2022"]])
+      expect(csv).to eq([["1111111"], ["2022"], [month]])
     end
-
   end
 
-  xdescribe "suggested return date is correct for months Feb to Dec" do
-  end
+  #xdescribe "suggested return date is correct for month: January" do
+  #end
 
-  xdescribe "suggested return date is correct for month: January" do
-  end
-
-  xdescribe "custom return date is required" do
-  end
+  #xdescribe "custom return date is required" do
+  #end
 end
