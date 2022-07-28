371ba2f70f03023154038f258a582289247bf51a
Kate Young <89363807+katemyoung@users.noreply.github.com>
2022-07-28 20:51:28 +0200

    Implement changes from PR review

--- spec/models/employer_spec.rb
+++ spec/models/employer_spec.rb
@@ -1,27 +1,19 @@
 require "rails_helper"
 
 describe Employer do
-  describe "initialisation" do
-    describe "valid case" do
-      it "has an account number" do
-        employer = create(:employer, account_number: 1234567)
-
-        expect(employer.account_number).to eq(1234567)
-      end
-
-      it "has an employer name" do
-        employer = create(:employer, name: "My Company")
-
-        expect(employer.name).to eq("My Company")
-      end
+  context "when all the correct attributes are assigned" do
+    it "is valid" do
+      employer = build_stubbed(:employer, account_number: 1234567, name: "Test company")
+      
+      expect(employer).to be_valid
     end
+  end
 
-    describe "invalid cases" do
-      it "fails when trying to create an employer with no account number" do
-        employer = build(:employer, account_number: nil)
+  context "invalid cases" do
+    it "fails when trying to create an employer with no account number" do
+      employer = build(:employer, account_number: nil)
 
-        expect(employer.save).to be(false)
-      end
+      expect(employer.save).to be(false)
     end
   end
 end
